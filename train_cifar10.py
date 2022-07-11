12a013752841a404c01d1aea0c96b6e43c007979
shansiliu95 <44488551+shansiliu95@users.noreply.github.com>
2022-07-11 16:56:51 -0700

    Update train_cifar10.py

--- train_cifar10.py
+++ train_cifar10.py
@@ -47,10 +47,10 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=1)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=10)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=1)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=10)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
